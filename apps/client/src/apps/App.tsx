aeec1832282cb6fe141a1211874fa351428612ca
jm8468 <jmtoyplay1113@gmail.com>
2026-01-09 07:22:31 +0900

    fix: EsLint 정렬 요청 반영

--- apps/client/src/apps/App.tsx
+++ apps/client/src/apps/App.tsx
@@ -1,9 +1,10 @@
+import { useState } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { useState } from 'react';
+import { RouterProvider } from 'react-router';
+
 import { createQueryClient } from '@shared/api/query-client';
 import { router } from '@shared/router/router';
-import { RouterProvider } from 'react-router';
 
 function App() {
   const [client] = useState(() => createQueryClient());
