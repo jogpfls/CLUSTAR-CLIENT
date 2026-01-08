c2c91fe77cd031863526d58bd4c623fdacae9893
jm8468 <jmtoyplay1113@gmail.com>
2026-01-09 07:28:54 +0900

    init: vite-plugin-svg-sprite 설정

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -2,8 +2,26 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 import tsconfigPaths from 'vite-tsconfig-paths';
+import svgSpritePlugin from '@pivanov/vite-plugin-svg-sprite';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgSpritePlugin({
+      iconDirs: [resolve(__dirname, '../../packages/cds-ui/src/icons')],
+      symbolId: 'icon-[name]',
+      inject: 'body-last',
+      svgoConfig: {
+        plugins: [
+          {
+            name: 'removeDimensions',
+            active: true,
+          },
+        ],
+      },
+    }),
+  ],
 });
