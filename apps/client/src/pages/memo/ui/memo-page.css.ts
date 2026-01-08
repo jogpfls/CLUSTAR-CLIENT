fa615cbf607b4cf2a366d5fa7123aaea0734e7e4
jogpfls <joerin4177@gmail.com>
2026-01-09 01:51:06 +0900

    fix: memo-page.css.ts에서 container 스타일 속성 추가

--- apps/client/src/pages/memo/ui/memo-page.css.ts
+++ apps/client/src/pages/memo/ui/memo-page.css.ts
@@ -5,4 +5,6 @@ import { themeVars } from '@cds/ui';
 export const container = style({
   ...themeVars.fontStyles.display_sb_36,
   backgroundImage: themeVars.color.gradient02,
+  color: themeVars.color.grey700,
+  width: '30rem',
 });
