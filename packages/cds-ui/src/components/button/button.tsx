88f1c3c6a9c2af825633f07144cf9b0706ded39e
백지연 <147830921+jyeon03@users.noreply.github.com>
2026-01-13 06:55:50 +0900

    Feat(cds): button 컴포넌트 구현 (#60)
    * feat: button 컴포넌트 구현
    
    * feat: button 컴포넌트 storybook 작성
    
    * fix: button 테스트코드 예시 파일 삭제
    
    * fix: 스토리북 구체화
    
    * fix: borderRadius 단위 변경
    
    * fix: type 위치 조정
    
    * feat: send icon 추가
    
    * feat: 버튼 디자인 추가
    
    * fix: 버튼 스토리북 수정
    
    * fix: 타입명 변경
    
    * fix: onCLick 옵셔널 제거
    
    * fix: 버튼 구조 수정
    
    * fix: 버튼 transition 수정
    
    * feat: 버튼 디자인 추가
    
    * fix: disabled 디자인 추가

--- packages/cds-ui/src/components/button/button.tsx
+++ packages/cds-ui/src/components/button/button.tsx
@@ -1,5 +1,42 @@
-const Button = () => {
-  return <button></button>;
+import type { ReactNode } from 'react';
+
+import * as styles from './button.css';
+
+export type ButtonSizeType = 'sm' | 'md' | 'lg' | 'xl';
+export type ButtonVariantType = 'solid' | 'outlined';
+
+export interface ButtonProps {
+  children?: ReactNode;
+  onClick: () => void;
+  size: ButtonSizeType;
+  variant?: ButtonVariantType;
+  disabled?: boolean;
+}
+
+const Button = ({
+  children,
+  onClick,
+  size,
+  variant = 'solid',
+  disabled = false,
+}: ButtonProps) => {
+  const applyOutlinedText =
+    size === 'md' && variant === 'outlined' && typeof children === 'string';
+
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      disabled={disabled}
+      className={styles.button({ size, variant, disabled })}
+    >
+      {applyOutlinedText ? (
+        <span className={styles.outlinedText}>{children}</span>
+      ) : (
+        children
+      )}
+    </button>
+  );
 };
 
 export default Button;
