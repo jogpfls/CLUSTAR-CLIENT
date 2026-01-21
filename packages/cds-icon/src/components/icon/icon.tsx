e9c5529e5d6dedc7589010c5933c066eedd1cfa4
jogpfls <joerin4177@gmail.com>
2026-01-22 02:13:46 +0900

    feat: Icon 컴포넌트를 named export로 변경

--- packages/cds-icon/src/components/icon/icon.tsx
+++ packages/cds-icon/src/components/icon/icon.tsx
@@ -10,7 +10,7 @@ interface IconProps extends SVGProps<SVGSVGElement> {
   ariaHidden?: boolean;
 }
 
-const Icon = ({
+export const Icon = ({
   name,
   width,
   height,
@@ -35,5 +35,3 @@ const Icon = ({
     </svg>
   );
 };
-
-export default Icon;
