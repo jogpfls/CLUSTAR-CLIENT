ca5383a29eef0388a1347dafb1fcb8b6ef7c65b0
임서준 <84189784+jm8468@users.noreply.github.com>
2026-01-23 16:13:12 +0900

    QA-1: 트리뷰 가운데 정렬 (#191)
    * fix: 구조화뷰 가운데 정렬
    
    * chore: 사용하지 않는 값 제거
    
    * chore: 주석 삭제
    
    * fix: 렌더링 시간 조정
    
    * Merge branch 'develop' into fix/tree-view-center-align/#169

--- apps/client/src/widgets/tree-view/ui/tree-view.css.ts
+++ apps/client/src/widgets/tree-view/ui/tree-view.css.ts
@@ -1,5 +1,34 @@
-import { style } from '@vanilla-extract/css';
+import { keyframes, style } from '@vanilla-extract/css';
+
+import { themeVars } from '@cds/ui';
 
 export const container = style({
   height: '100vh',
+});
+
+const rotateLoading = keyframes({
+  '0%': {
+    transform: 'rotate(0deg)',
+  },
+  '100%': {
+    transform: 'rotate(360deg)',
+  },
+});
+
+export const loadingContainer = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+  width: '100%',
+});
+
+export const spinner = style({
+  width: '4rem',
+  height: '4rem',
+  borderRadius: '50%',
+  border: '3px solid transparent',
+  borderTopColor: themeVars.color.blue500,
+  borderBottomColor: themeVars.color.blue500,
+  animation: `${rotateLoading} 1s linear infinite`,
 });
